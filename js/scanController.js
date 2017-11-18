17a91e0b02aa886fd569729a8d4bbb37b3e10ccf
RomainCourtin <rc.courtin@gmail.com>
2017-11-18 18:13:50 +0100

    First commit

---
+++ js/scanController.js
@@ -0,0 +1,28 @@
+function scan() {
+
+  $.ajax({ 
+    type: "GET",
+    dataType: "jsonp",
+    url: "http://localhost:8080/scan",
+    success: function(data){        
+     formatResult(data);
+    }
+  });
+}
+
+function formatResult(data){
+  var formattedResults = "";
+  
+  var jsonObject = jQuery.parseJSON(data);
+  $.each( jsonObject, function( index, node ){
+    var htmlNode = '<div id="node'+index+'" class="node"><img src="img/smartphone-call.svg" alt="phone" class="smallImage"/>';
+    if(node.hostname) {htmlNode += '<p>'+node.hostname+'</p>';}
+    if(node.ip) {htmlNode += '<p>'+node.ip+'</p>';}
+    if(node.mac) {htmlNode += '<p>'+node.mac+'</p>';}
+    htmlNode += '<p><a href="#" class="btn btn-sm btn-secondary">action1</a>&nbsp;';
+    htmlNode += '<a href="#" class="btn btn-sm btn-secondary">action2</a></p></div>';
+    formattedResults = formattedResults + htmlNode;
+  });
+
+  $("#mainContent").html( formattedResults);
+}
